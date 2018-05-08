a168c9490854f3af983bebcc416d35520983cd4a
MandaloreXIII <samleboy08@gmail.com>
2018-05-08 02:12:26 -0500

    check of iteration functoin

--- race.js
+++ race.js
@@ -1,11 +1,14 @@
 function race() {
   var winner = null;
+  var racer1 = document.getElementById("racer1");
+  var racer2 = document.getElementById("racer2");
   do {
     // Sets the speeds of the racers.
-    var racer1_speed = Math.floor(Math.random()*(3)) + 4;
-    var racer2_speed = Math.floor(Math.random()*(3)) + 4;
+    var racer1_speed = Math.floor(Math.random()*(41)) + 80;
+    var racer2_speed = Math.floor(Math.random()*(41)) + 80;
     // INSERT THE ITERATION OF CARS ON SCREEN
-
+    racer1.left = racer1.left + racer1_speed;
+    racer2.left = racee2.left + racer2_speed;
     //Check if car passed finish line
     if ()
   }
