eebde60c5966f259943571ab608fc8c14d421c41
Salt <rehashedsalt@cock.li>
2018-05-08 13:33:43 -0500

    race.js: BIG TABS

--- race.js
+++ race.js
@@ -1,27 +1,25 @@
 function race() {
-  var winner = null;
-  var racer1 = document.getElementById("racer1");
-  var racer2 = document.getElementById("racer2");
-  var eurobeat = document.getElementById("eurobeat");
-  eurobeat.volume = 0.2;
-  eurobeat.play();
-  do {
-    // Sets the speeds of the racers.
-    var racer1_speed = Math.floor(Math.random()*(0.04)) + 0.03;
-    var racer2_speed = Math.floor(Math.random()*(0.04)) + 0.03;
-    
-    // Transition them using CSS
-    // TODO: That.
-    
-    //Check if car passed finish line
-    if (racer1.style.left.toLocaleString("en", {style: "decimial"}) > 0.9) {
-      winner = "racer1";
-    } else if (racer2.style.left.toLocaleString("en", {style: "decimial"}) > 0.9) {
-      winner = "racer2";
-    }
-  }
-   while (true);
-  eurobeat.pause();
-  eurobeat.currentTime = 0;
-  return 0;
+	var winner = null;
+	var racer1 = document.getElementById("racer1");
+	var racer2 = document.getElementById("racer2");
+	var eurobeat = document.getElementById("eurobeat");
+	eurobeat.volume = 0.2;
+	eurobeat.play();
+	do {
+		// Sets the speeds of the racers.
+		var racer1_speed = Math.floor(Math.random()*(0.04)) + 0.03;
+		var racer2_speed = Math.floor(Math.random()*(0.04)) + 0.03;
+
+		// Transition them using CSS
+
+		//Check if car passed finish line
+		if (racer1.style.left.toLocaleString("en", {style: "decimial"}) > 0.9) {
+			winner = "racer1";
+		} else if (racer2.style.left.toLocaleString("en", {style: "decimial"}) > 0.9) {
+			winner = "racer2";
+		}
+	} while (true);
+	eurobeat.pause();
+	eurobeat.currentTime = 0;
+	return 0;
 }
