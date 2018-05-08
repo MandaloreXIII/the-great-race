d18e2e9574d5bd67ad04877ca97e6ccb6e3babdc
Salt <rehashedsalt@cock.li>
2018-05-08 13:31:54 -0500

    Index: Fix stoplight bug, also fuck up the javascript

--- race.js
+++ race.js
@@ -6,25 +6,13 @@ function race() {
   eurobeat.volume = 0.2;
   eurobeat.play();
   do {
-    console.log("Attempting movement iteration");
-    var racer1_current_pos = document.getElementById("racer1").style.left;
-    var racer2_current_pos = document.getElementById("racer2").style.left;
-    console.log("racer1 starts at: " + racer1_current_pos);
-    console.log("racer2 starts at: " + racer2_current_pos);
     // Sets the speeds of the racers.
     var racer1_speed = Math.floor(Math.random()*(0.04)) + 0.03;
     var racer2_speed = Math.floor(Math.random()*(0.04)) + 0.03;
-    racer1_speed = racer1_speed.toLocaleString("en", {style: "percent"});
-    racer2_speed = racer2_speed.toLocaleString("en", {style: "percent"});
-    console.log("Racer 1: " + racer1_speed + " Racer 2: " + racer2_speed);
-    console.log("Racer 1: " + racer1_speed + " Racer 2: " + racer2_speed);
-    var racer1_new_pos = racer1_current_pos + racer1_speed;
-    var racer2_new_pos = racer2_current_pos + racer2_speed;
-    //TODO: THE ITERATION OF CARS ON SCREEN
-    console.log("Racer 1 pos: " + racer1.style.left + " Racer 2 pos: " + racer2.style.left);
-    racer1.style.left = racer1_new_pos;
-    racer2.style.left = racer2_new_pos;
-    console.log("Racer 1 pos: " + racer1.style.left + " Racer 2 pos: " + racer2.style.left);
+    
+    // Transition them using CSS
+    // TODO: That.
+    
     //Check if car passed finish line
     if (racer1.style.left.toLocaleString("en", {style: "decimial"}) > 0.9) {
       winner = "racer1";
