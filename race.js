2a865fe2dcd5248585431b0fb1bccf069a8e4a05
MandaloreXIII <samleboy08@gmail.com>
2018-05-08 01:39:22 -0500

    futher iteration of race.js

--- race.js
+++ race.js
@@ -1,3 +1,8 @@
 function race() {
-
+  var winner = null;
+  do {
+    var racer1_speed = Math.floor(Math.random()*(3)) + 4;
+    var racer2_speed = Math.floor(Math.random()*(3)) + 4;
+  }
+   while (winner = null);
 }
