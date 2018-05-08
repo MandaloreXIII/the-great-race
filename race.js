9f9117114d0a95716c575d2a315f59426133b492
MandaloreXIII <samleboy08@gmail.com>
2018-05-08 02:30:47 -0500

    fixing the thing

--- race.js
+++ race.js
@@ -6,9 +6,9 @@ function race() {
     // Sets the speeds of the racers.
     var racer1_speed = Math.floor(Math.random()*(4)) + 3;
     var racer2_speed = Math.floor(Math.random()*(4)) + 3;
-    racer1_speed.toLocaleString("en", {style: "percent"});
-    racer2_speed.toLocaleString("en", {style: "percent"});
-    // INSERT THE ITERATION OF CARS ON SCREEN
+    racer1_speed = racer1_speed.toLocaleString("en", {style: "percent"});
+    racer2_speed = racer2_speed.toLocaleString("en", {style: "percent"});
+    //TODO: THE ITERATION OF CARS ON SCREEN
     racer1.left += racer1_speed;
     racer2.left += racer2_speed;
     //Check if car passed finish line
