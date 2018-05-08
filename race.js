0dc7645e5474bdeb89c3a68b1b99f4bc242fea88
MandaloreXIII <samleboy08@gmail.com>
2018-05-08 01:48:24 -0500

    Added comments

--- race.js
+++ race.js
@@ -1,8 +1,13 @@
 function race() {
   var winner = null;
   do {
+    // Sets the speeds of the racers.
     var racer1_speed = Math.floor(Math.random()*(3)) + 4;
     var racer2_speed = Math.floor(Math.random()*(3)) + 4;
+    // INSERT THE ITERATION OF CARS ON SCREEN
+
+    //Check if car passed finish line
+
   }
    while (winner = null);
 }
