13e66beab69406be3f6e608a8343143ece337174
Salt <rehashedsalt@cock.li>
2018-05-08 02:13:45 -0500

    race.js: Small syntactic change

--- race.js
+++ race.js
@@ -7,8 +7,8 @@ function race() {
     var racer1_speed = Math.floor(Math.random()*(41)) + 80;
     var racer2_speed = Math.floor(Math.random()*(41)) + 80;
     // INSERT THE ITERATION OF CARS ON SCREEN
-    racer1.left = racer1.left + racer1_speed;
-    racer2.left = racee2.left + racer2_speed;
+    racer1.left += racer1_speed;
+    racer2.left += racer2_speed;
     //Check if car passed finish line
     if ()
   }
